34d3a32baecfb53a06e6a26f144e221a40e1c7b7
mohammad <mo.iz1377@gmail.com>
2023-04-11 20:09:47 +0330

    complete accounts models, admin, forms

--- accounts/admin.py
+++ accounts/admin.py
@@ -11,3 +11,20 @@ class CustomUserAdmin(UserAdmin):
     model = CustomUser
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
+    list_display = ("name", "email", "phone_number", "birthday", "sex")
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": ("name", "phone_number", "birthday", "sex")
+            }),
+    ) + UserAdmin.fieldsets
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("name", "phone_number", "birthday", "sex", "password1", "password2"),
+            },
+        ),
+    )
--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,44 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from django.utils.translation import gettext_lazy as _
+from .custom_user_manager import CustomUserManager
 
 # Create your models here.
 
 
 class CustomUser(AbstractUser):
-    pass
+    SEX_CHOICES = (
+        ('male', 'male'),
+        ('female', 'female'),
+    )
+    username = models.CharField(_('username'), max_length=50, blank=True)
+    phone_number = models.CharField(
+        _('phone number'),
+        unique=True,
+        max_length=11,
+        validators=[
+            RegexValidator(
+                regex=r"(09\d{9})",
+                message="Phone number must be entered in the format '09000000000'. Up to 11 digits allowed.",
+            )
+        ]
+    )
+    name = models.CharField(_("name"), max_length=150)
+    birthday = models.DateField(_('birthday'))
+    sex = models.CharField(
+        _('sex'),
+        max_length=6,
+        choices=SEX_CHOICES,
+        validators=[
+            RegexValidator(
+                regex=r"(male)|(female)",
+                message="Sex must be one of these two options 'male or female'.",
+            )
+        ]
+    )
+
+    object = CustomUserManager()
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = ["name", "birthday", "sex"]
+    
